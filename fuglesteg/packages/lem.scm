60e52b9b27c49ed6c3883596017167f946f87de6
Andreas Fuglesteg Dale <andreasfdale@gmail.com>
2025-02-03 19:57:14 +0100

    Fix newest version of lem

--- fuglesteg/packages/lem.scm
+++ fuglesteg/packages/lem.scm
@@ -1,5 +1,8 @@
 (define-module (fuglesteg packages lem)
   #:use-module (guix packages)
+  #:use-module (guix gexp)
+  #:use-module (guix transformations)
+  #:use-module (guix git-download)
   #:use-module (gnu packages wm)
   #:use-module (gnu packages lisp-xyz)
   #:use-module (gnu packages text-editors)
@@ -7,21 +10,25 @@
   #:use-module (guix build-system asdf)
   #:use-module ((guix licenses) #:prefix license:))
 
-(define-public lem
-  (package
-    (inherit lem)
-    (source
-       (origin
-         (method git-fetch)
-         (uri (git-reference
-               (url "https://github.com/lem-project/lem/")
-               (commit commit)))
-         (sha256
-          (base32 "0bmwcj1hdp5yi19afxg13rhkbcf6il8fqh81q55winqc35d0h6mn"))
-         (file-name (git-file-name name version))
-         (snippet
-          #~(begin
-              (use-modules (guix build utils))
-              (delete-file-recursively "roswell")
-              ;; Delete precompiled shared object files.
-              (delete-file-recursively "extensions/terminal/lib")))))))
+(define-public lem-latest
+               (let ((revision "1")
+                     (commit "672a649674d3c75b91b3da777df0d490a4bb92a0"))
+                 (package
+                   (inherit lem)
+                   (name "lem")
+                   (version (git-version "2.3.0" revision commit))
+                   (source
+                     (origin
+                       (method git-fetch)
+                       (uri (git-reference
+                              (url "https://github.com/lem-project/lem/")
+                              (commit commit)))
+                       (sha256
+                         (base32 "1fscqlh10dhh9p642l8v46ilzazy4b61p2pk8kld2dnl1kj6kxsf"))
+                       (file-name (git-file-name name version))
+                       (snippet
+                         #~(begin
+                             (use-modules (guix build utils))
+                             (delete-file-recursively "roswell")
+                             ;; Delete precompiled shared object files.
+                             (delete-file-recursively "extensions/terminal/lib"))))))))
