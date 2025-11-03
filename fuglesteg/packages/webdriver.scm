a3a5d3efda6ff39c6b29ae78e468a6572e5f4b12
Andreas Fuglesteg Dale <andreasfdale@gmail.com>
2025-11-03 15:14:14 +0100

    Add sbcl-cl-webdriver-client webdriver.scm

---
+++ fuglesteg/packages/webdriver.scm
@@ -0,0 +1,42 @@
+(define-module (fuglesteg packages webdriver)
+  #:use-module (guix packages)
+  #:use-module (guix gexp)
+  #:use-module (guix transformations)
+  #:use-module (guix git-download)
+  #:use-module (gnu packages wm)
+  #:use-module (gnu packages webkit)
+  #:use-module (gnu packages pkg-config)
+  #:use-module (gnu packages documentation)
+  #:use-module (gnu packages graphviz)
+  #:use-module (gnu packages lisp-check)
+  #:use-module (gnu packages terminals)
+  #:use-module (gnu packages lisp-xyz)
+  #:use-module (gnu packages text-editors)
+  #:use-module (guix git-download)
+  #:use-module (guix build-system asdf)
+  #:use-module (guix build-system cmake)
+  #:use-module ((guix licenses) #:prefix license:))
+
+(define sbcl-cl-webdriver-client
+  (let ((commit "3c2c377fe548504071cbd29c7ba8b9ccb8588a74")
+        (revision "0"))
+  (package
+   (name "cl-webdriver-client")
+   (version (git-version "0.1" revision commit))
+   (source
+    (origin
+     (method git-fetch)
+     (uri (git-reference
+           (url "https://github.com/copyleft/cl-webdriver-client")
+           (commit commit)))
+     (sha256
+      (base32 "1kn5hlgdsbxjk7gkv6gqsc20cfkgc04kzdxpjysnbf9v678r0g3b"))
+     (file-name (git-file-name name version))))
+   (build-system asdf-build-system/sbcl)
+   (inputs (list sbcl-dexador
+                 sbcl-quri
+                 sbcl-cl-json
+                 sbcl-alexandria
+                 sbcl-split-sequence
+                 sbcl-assoc-utils))
+   (description "") (synopsis "") (home-page "") (license license:expat))))
