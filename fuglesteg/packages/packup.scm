f3316f772c4871d2ee857bd68eef2fedf18bba52
Andreas Fuglesteg Dale <andreasfdale@gmail.com>
2025-11-20 11:50:07 +0100

    Packup: Update

--- fuglesteg/packages/packup.scm
+++ fuglesteg/packages/packup.scm
@@ -4,20 +4,21 @@
   #:use-module (guix git-download)
   #:use-module (guix build-system copy)
   #:use-module ((guix licenses) #:prefix license:)
+  #:use-module (gnu packages ssh)
   #:use-module (gnu packages lisp)
   #:use-module (gnu packages rsync))
 
 (define-public packup
   (package
    (name "packup")
-   (version "1.2")
+   (version "1.3")
    (source
     (origin
      (method git-fetch)
      (uri (git-reference
            (url "https://github.com/Fuglesteg/packup")
-           (commit "58c687a360e54cc82dfe9d5bac9ced8504f10928")))
-     (sha256 (base32 "1d2gc4rhsbmq32d7hrrkdfdl15n4zib046rcxmk9sbgvyixb1c6g"))
+           (commit "5fcbbe5a49ad7235adedd1d3a19f609918a9652b")))
+     (sha256 (base32 "14awasjkm1nwyh2c82wgizhvxcx0wg33rx57vdc5lb871s5alyr5"))
      (file-name (git-file-name name version))))
    (build-system copy-build-system)
    (arguments
@@ -27,9 +28,9 @@
                                            (lambda _
                                              (substitute* "packup.lisp"
                                                           (("^#!.*") (string-append "#!" #$sbcl "/bin/sbcl --script"))
+                                                          (("ssh") (string-append #$openssh "/bin/ssh"))
                                                           (("rsync") (string-append #$rsync "/bin/rsync"))))))
      #:install-plan #~(list '("packup.lisp" "bin/packup"))))
-   (inputs (list sbcl rsync))
+   (inputs (list sbcl rsync openssh))
    (license license:expat)
    (synopsis "") (description "") (home-page "")))
-          
